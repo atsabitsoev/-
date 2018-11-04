768a0acf6228b176d34722f83920375dfa11fb63
Atsamaz <atsamazbitsoev@yandex.ru>
2018-11-04 17:48:51 +0300

    Initial Commit

---
+++ Бомонка/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Бомонка
+//
+//  Created by Ацамаз on 04/11/2018.
+//  Copyright © 2018 a.s.bitsoev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
